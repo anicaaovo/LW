b3cafa8618a2985733c28845bb4fc3b7dc4af940
anicaa <kz245@rutgers.edu>
2023-01-15 04:10:58 -0500

    demo 1

--- LsBackend/src/main/java/com/ls/lsbackend/controller/pk/BotInfoController.java
+++ LsBackend/src/main/java/com/ls/lsbackend/controller/pk/BotInfoController.java
@@ -3,6 +3,9 @@ package com.ls.lsbackend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
@@ -13,8 +16,11 @@ public class BotInfoController {
      * @return 字符串类型的bot信息。
      */
     @RequestMapping("botinfo/")
-    public String botInfo(){
-        return null;
+    public Map<String, String> botInfo(){
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name","tiger");
+        bot1.put("level","999");
+        return bot1;
     }
 }
 
